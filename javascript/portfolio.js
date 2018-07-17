f388f681e521250a1de48c0bd0c45198b1a83376
Adelita Esparza <AESPARZA051@GMAIL.COM>
2018-07-17 15:26:51 -0500

    redo
    Updated entire portfolio using bootstrap4.

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -1,30 +1,22 @@
-// Get the modal
-var modal = document.getElementById('myModal');
-
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+$(document).ready(function(){
+  $('.nav-button').click(function(){
+    $('.nav-button').toggleClass('change');
+  });
+  $(window).scroll(function(){
+  let position = $(this).scrollTop();
+  if (position >= 200) {
+    $('.nav-menu').addClass('custom-navbar');
+  } else {$('.nav-menu').removeClass('custom-navbar');
+  }
+});
+$(window).scroll(function () {
+    let position = $(this).scrollTop();
+    if (position >= 650){
+      $('.ae-img').addClass('fromLeft');
+      $('.hobbies-text').addClass('fromRight');
+    } else{
+      $('.ae-img').removeClass('fromLeft');
+      $('.hobbies-text').removeClass('fromRight');
     }
-}
-$('.popover-dismiss').popover({
-  trigger: 'focus'
-})
+  });
+});
